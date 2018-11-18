af269cd85f6d88b786ae4a57e07f4431c69b9883
adan_eslavo <adan_eslavo@o2.pl>
2018-11-19 00:07:23 +0100

    0.3
    Added few civ renaming and some detailed components.

--- Settings/ACNSettings.sql
+++ Settings/ACNSettings.sql
@@ -2,9 +2,20 @@
 Alternative Names!
 0 = Mixed (original version).
 1 = Endonyms (english names).
-2 = Xenonyms (native names; set as default).
+2 = Xenonyms (native names).
+3 = Xenonyms-detailed (native names including words very similar (load words); set as default).
 */
 
 INSERT INTO COMMUNITY	
 		(Type,			Value)
-VALUES	('ACN',			2);
+VALUES	('ACNALT',		3);
+
+/*
+Civilization's name renaming!
+0 = Original (original version).
+1 = Xenonyms (native names; set as default).
+*/
+
+INSERT INTO COMMUNITY	
+		(Type,			Value)
+VALUES	('ACNCIV',		1);
