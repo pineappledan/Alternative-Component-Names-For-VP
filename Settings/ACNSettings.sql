054420fc294b90fbf57ac267748cfc257570d7dd
adan_eslavo <adan_eslavo@o2.pl>
2018-11-18 21:15:42 +0100

    v0.2
    Cleaning. Added checks.

---
+++ Settings/ACNSettings.sql
@@ -0,0 +1,10 @@
+/*
+Alternative Names!
+0 = Mixed (original version).
+1 = Endonyms (english names).
+2 = Xenonyms (native names; set as default).
+*/
+
+INSERT INTO COMMUNITY	
+		(Type,			Value)
+VALUES	('ACN',			2);
